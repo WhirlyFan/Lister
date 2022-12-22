b2550c3e9b42f2586b1f36455938e7d7acaa526c
Michael Lee <mhykol2114@gmail.com>
2022-12-21 22:29:09 -0800

    finished backend review routes

---
+++ app/forms/review_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from app.models import Review
+
+class CreateReview(FlaskForm):
+    review = StringField('review', validators=[DataRequired()])
+    rating = StringField('rating', validators=[DataRequired()])
+
+class UpdateReview(FlaskForm):
+    review = StringField('review', validators=[DataRequired()])
+    rating = StringField('rating', validators=[DataRequired()])
