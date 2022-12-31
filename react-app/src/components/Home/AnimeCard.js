ae92a0e8cde2ae7434d08289b812c89bf3f4292a
Michael Lee <mhykol2114@gmail.com>
2022-12-31 14:22:17 -0800

    added anime details page

--- react-app/src/components/Home/AnimeCard.js
+++ react-app/src/components/Home/AnimeCard.js
@@ -1,8 +1,20 @@
 import styles from "./Home.module.css";
+import { useHistory } from "react-router-dom";
 
 export default function AnimeCard({ anime, index }) {
+  const history = useHistory();
+
+  const animeDetails = (id) => {
+    history.push(`/anime/${id}`);
+  };
+
   return (
-    <div className={styles.anime}>
+    <div
+      className={styles.anime}
+      onClick={() => {
+        animeDetails(anime.mal_id);
+      }}
+    >
       <div>
         {`${index + 1}: `}
         {anime.title}
