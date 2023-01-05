96550fabcd7c71b98087b4895cb0cb470a98c3b3
Michael Lee <mhykol2114@gmail.com>
2023-01-05 14:29:56 -0800

    added demo user button

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login } from "../../store/session";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onLogin = async (e) => {
@@ -27,37 +27,57 @@ const LoginForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
+
+  const demoButton = () => {
+    setEmail("demo@aa.io");
+    setPassword("password");
+    dispatch(login(email, password)).then((data) => {
+      if (data) {
+        setErrors(data);
+      }
+    });
+  };
 
   return (
     <form onSubmit={onLogin}>
       <div>
         {errors.map((error, ind) => (
-          <div key={ind} className="error">{error}</div>
+          <div key={ind} className="error">
+            {error}
+          </div>
         ))}
       </div>
       <div>
-        <label htmlFor='email'>Email</label>
+        <label htmlFor="email">Email</label>
         <input
-          name='email'
-          type='email'
-          placeholder='Email'
+          name="email"
+          type="email"
+          placeholder="Email"
           value={email}
           onChange={updateEmail}
         />
       </div>
       <div>
-        <label htmlFor='password'>Password</label>
+        <label htmlFor="password">Password</label>
         <input
-          name='password'
-          type='password'
-          placeholder='Password'
+          name="password"
+          type="password"
+          placeholder="Password"
           value={password}
           onChange={updatePassword}
         />
-        <button type='submit'>Login</button>
+        <button type="submit">Login</button>
       </div>
+      <button
+        type="button"
+        onClick={() => {
+          demoButton();
+        }}
+      >
+        Log in with Demo User
+      </button>
     </form>
   );
 };
