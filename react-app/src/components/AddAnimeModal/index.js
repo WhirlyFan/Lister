965f3a38b7e5576e45591f62e758856f07fd21d9
Michael Lee <mhykol2114@gmail.com>
2023-01-06 12:49:24 -0800

    added isLoaded to add to list modal

--- react-app/src/components/AddAnimeModal/index.js
+++ react-app/src/components/AddAnimeModal/index.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Modal } from "../../context/Modal";
 import AddAnimeForm from "./AddAnimeForm";
+import { getListsThunk } from "../../store/lists";
 
 function AddAnimeModal({ anime, list, listMode, setList }) {
+  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
+  const user = useSelector((state) => state.session.user);
 
   return (
     <>
-      <button onClick={() => setShowModal(true)}>
+      <button
+        onClick={() => {
+          dispatch(getListsThunk(user.id)).then(() => {
+            setShowModal(true);
+          });
+        }}
+      >
         {listMode ? "Edit" : "Add to List"}
       </button>
       {showModal && (
