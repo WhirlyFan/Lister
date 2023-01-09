cd5bf716029a2b3166d77de6bb42814fd5df5858
Michael Lee <mhykol2114@gmail.com>
2023-01-08 18:13:09 -0800

    added isLoaded to profile page

--- react-app/src/components/Profile/Profile.js
+++ react-app/src/components/Profile/Profile.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Profile() {
   const [user, setUser] = useState({});
-  const { userId }  = useParams();
+  const { userId } = useParams();
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     if (!userId) {
@@ -13,15 +14,17 @@ export default function Profile() {
       const response = await fetch(`/api/users/${userId}`);
       const user = await response.json();
       setUser(user);
-    })();
+    })().then(() => {
+      setIsLoaded(true);
+    });
   }, [userId]);
 
-  if (!user) {
+  if (!user || !isLoaded) {
     return null;
   }
 
   return (
-    <div >
+    <div>
       <h1>{`${user.username}'s Profile`}</h1>
       <h2>Feature Coming Soon...</h2>
     </div>
