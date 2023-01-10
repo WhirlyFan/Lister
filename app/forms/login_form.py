770af942e916f95933a4f61431115e50e0abe0ff
Michael Lee <mhykol2114@gmail.com>
2023-01-09 16:08:20 -0800

    added length validators

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -24,6 +24,9 @@ def password_matches(form, field):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[
-                           DataRequired(), password_matches])
+    email = StringField('email', validators=[DataRequired(), user_exists, Length(
+        min=3, max=30, message='Email must be between 3 and 30 characters long.'
+    )])
+    password = StringField('password', validators=[DataRequired(), password_matches, Length(
+        min=3, max=30, message='Password must be between 3 and 30 characters long.'
+    )])
--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -6,12 +6,12 @@ from app.models import Review
 
 def rating_validator(form, field):
     if not (1 <= int(field.data) <= 10):
-        raise ValidationError('Rating must be between 1 and 10')
+        raise ValidationError('Rating must be between 1 and 10.')
 
 
 class CreateReview(FlaskForm):
     review = StringField('review', validators=[DataRequired(), Length(
-        min=3, max=255, message='Review must be between 3 and 255 characters long')])
+        min=3, max=255, message='Review must be between 3 and 255 characters long.')])
     rating = StringField('rating', validators=[
                          DataRequired(), rating_validator])
     anime_id = IntegerField('anime_id', validators=[DataRequired()])
@@ -20,7 +20,7 @@ class CreateReview(FlaskForm):
 
 class UpdateReview(FlaskForm):
     review = StringField('review', validators=[DataRequired(), Length(
-        min=3, max=255, message='Review must be between 3 and 255 characters long')])
+        min=3, max=255, message='Review must be between 3 and 255 characters long.')])
     rating = StringField('rating', validators=[
                          DataRequired(), rating_validator])
     submit = SubmitField('Update Review')
