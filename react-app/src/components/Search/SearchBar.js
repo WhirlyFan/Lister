d2b678b302d5d17d90ac0b06a70437c4e512f846
Michael Lee <mhykol2114@gmail.com>
2023-01-16 23:34:39 -0800

    temp removed user search. added anime search

--- react-app/src/components/Search/SearchBar.js
+++ react-app/src/components/Search/SearchBar.js
@@ -9,37 +9,46 @@ export default function SearchBar() {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (e.target.category.value === "Anime" && e.target.search.value.length) {
+  //     dispatch(searchThunk(e.target.search.value)).then((data) => {
+  //       if (data.status) {
+  //         setTimeout(() => {
+  //           dispatch(searchThunk(e.target.search.value));
+  //         }, 1500);
+  //       } else {
+  //         history.push("/search/animes");
+  //       }
+  //     }); //might have to add an anime argument here for manga integration later
+  //   }
+  //   if (e.target.category.value === "User" && e.target.search.value.length) {
+  //     dispatch(getUsersThunk(e.target.search.value)).then(() => {
+  //       history.push("/search/users");
+  //     });
+  //   }
+  // };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (e.target.category.value === "Anime" && e.target.search.value.length) {
-      dispatch(searchThunk(e.target.search.value)).then((data) => {
-        if (data.status) {
-          setTimeout(() => {
-            dispatch(searchThunk(e.target.search.value));
-          }, 1500);
-        } else {
-          history.push("/search/animes");
-        }
-      }); //might have to add an anime argument here for manga integration later
-    }
-    if (e.target.category.value === "User" && e.target.search.value.length) {
-      dispatch(getUsersThunk(e.target.search.value)).then(() => {
-        history.push("/search/users");
-      });
-    }
+    dispatch(searchThunk(e.target.search.value)).then((data) => {
+      if (data.status) {
+        setTimeout(() => {
+          dispatch(searchThunk(e.target.search.value));
+        }, 1500);
+      } else {
+        history.push("/search/animes");
+      }
+    }); //might have to add an anime argument here for manga integration later
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles.search_bar}>
-      <select name="category">
+      {/* <select name="category">
         <option>Anime</option>
         <option>User</option>
-      </select>
-      <input
-        name="search"
-        type="text"
-        placeholder="Search for Animes or Users!"
-      />
+      </select> */}
+      <input name="search" type="text" placeholder="Search for Animes" />
       <button type="submit">
         <i className="fas fa-search"></i>
       </button>
